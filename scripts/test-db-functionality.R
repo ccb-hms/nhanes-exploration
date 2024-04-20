a07922139efc8e7da11e1b4eb57d037b0213bbbb
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2024-04-20 18:48:01 +0530

    script to test basic DB functionality

---
+++ scripts/test-db-functionality.R
@@ -0,0 +1,25 @@
+
+library(nhanesA)
+
+(con <- nhanesA:::cn())
+
+if (!is(cn(), "DBIConnection")) stop("Not connected to DB")
+
+
+(tdemo <- setdiff(nhanesSearchTableNames("DEMO"), "P_DEMO"))
+
+system.time(dlist0 <- lapply(tdemo, nhanes, translated = FALSE))
+system.time(dlist0 <- lapply(tdemo, nhanes, translated = FALSE))
+system.time(dlist1 <- lapply(tdemo, nhanes, translated = TRUE))
+system.time(dlist1 <- lapply(tdemo, nhanes, translated = TRUE))
+
+common_vars0 <- lapply(dlist0, names) |> Reduce(f = intersect)
+common_vars1 <- lapply(dlist1, names) |> Reduce(f = intersect)
+
+identical(sort(common_vars0), sort(common_vars1))
+common_vars0 == common_vars1
+
+lapply(dlist0, `[`, common_vars0) |> do.call(what = rbind) |> str()
+lapply(dlist1, `[`, common_vars1) |> do.call(what = rbind) |> str()
+
+
